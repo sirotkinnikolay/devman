4bec710791be4b3f8862fc37c87f2cf02fe37c38
sirotkin <sirotkin.nikola@mail.ru>
2023-06-09 00:30:35 +0300

    Improvement 3

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
 import requests
 
-urls = ['https://wttr.in//London?n?M?qTqu&lang=ru', 'https://wttr.in//svo?n?M?qTqu&lang=ru',
-        'https://wttr.in//Череповец?n?M?qTqu&lang=ru'
+site = "https://wttr.in//"
+
+urls = [
+    'London?n?M?qTqu&lang=ru',
+    'svo?n?M?qTqu&lang=ru',
+    'Череповец?n?M?qTqu&lang=ru'
         ]
 for url in urls:
-    response = requests.get(url)
+    response = requests.get(f'{site}{url}')
     print(response.text)
     print('-' * 63)
